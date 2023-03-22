cbbacf0be7805b8dc999d19a1d05ef3082426c5b
wildenthal <8503469+wildenthal@users.noreply.github.com>
2023-03-22 21:42:07 +0100

    separates grid operations into module

---
+++ src/grid_ops.f90
@@ -0,0 +1,76 @@
+module grid_ops
+      implicit none
+      private
+      
+      integer, parameter :: K = kind(0.d0)
+      integer, parameter :: file_number = 11
+      real(K) :: xL, yL
+      integer :: n
+      public initialize_grid, save_grid, xpos, xint
+
+      contains
+      
+      ! general grid operations
+      subroutine initialize_grid(grid,lengthX,lengthY,dots)
+            real(K), allocatable :: grid(:,:)
+            real(K) :: lengthX, lengthY
+            integer :: i,j, dots
+            allocate(grid(n,n))
+            do j = 1, n
+                  do i = 1, n
+                        grid(i,j) = 0.0_K
+                  end do
+            end do
+            xL = lengthX
+            yL = lengthY
+            n = dots
+      end
+
+      subroutine save_grid(grid)
+            integer :: i,j
+            real(K) :: axis_value(2)
+            real(K), dimension(n,n) :: grid
+            open(unit=file_number, file='grid.dat', status='replace')
+            do j = 1, n
+                  do i = 1, n
+                        write(file_number, '(f10.3)',advance='no') grid(i,j)
+                        write(file_number, '(a)', advance='no') ' '
+                  end do
+                  write(file_number,*)
+            end do
+            close(file_number)
+
+            open(unit=file_number,file='x_axis.dat', status='replace')
+            do i = 1, n
+                  axis_value = xpos((/i,1/),grid)
+                  write(file_number, *) axis_value(1)
+            end do
+            close(file_number)
+
+            open(unit=file_number,file='y_axis.dat', status='replace')
+            do j = 1, n
+                  axis_value = xpos((/1,j/),grid)
+                  write(file_number, *) axis_value(2)
+            end do
+            close(file_number)
+
+      end
+
+      function xpos(integer_position,grid)
+            integer, dimension(2) :: integer_position
+            real(K), dimension(2) :: xpos
+            real(K), dimension(n,n) :: grid
+            
+            xpos(1) = -xL/2.0_K + xL*(integer_position(1)-1)/(n-1)
+            xpos(2) = -yL/2.0_K + yL*(integer_position(2)-1)/(n-1)
+      end
+
+      function xint(x,grid)
+            real(K), dimension(2) :: x
+            integer, dimension(2) :: xint
+            real(K), dimension(n,n) :: grid
+
+            xint(1) = (x(1)/xL + 0.5_K)*(n-1)+1
+            xint(2) = (x(1)/xL + 0.5_K)*(n-1)+1
+      end 
+end
--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,5 @@
 program cheerios
+      use grid_ops 
       implicit none
       
       !general parameters
@@ -12,7 +13,7 @@ program cheerios
       real(K), parameter :: yL = 10.0_K
       
       ! grid variables
-      real(K) :: grid(n,n)
+      real(K), allocatable :: grid(:,:)
 
       ! sphere variables
       type sphere
@@ -26,77 +27,25 @@ program cheerios
       ! initializations
       type(sphere) :: sphere1
       type(sphere) :: sphere2
+      call initialize_grid(grid,xL,yL,n)
+      print *, "grid done!"
+      print *, grid(1,1)
       sphere1%coordinates = (/2.0_K,2.0_K/)
-      sphere1%intcoord = xint(sphere1%coordinates)
+      sphere1%intcoord = xint(sphere1%coordinates,grid)
       sphere1%height = 0.25_K
       sphere2%coordinates = (/2.5_K,2.5_K/)
-      sphere2%intcoord = xint(sphere2%coordinates)
+      sphere2%intcoord = xint(sphere2%coordinates,grid)
       sphere2%height = 0.25_K
-      call initialize_grid()
+
       
       ! operations
       call make_dip(sphere1)
       !call make_dip(sphere2)
-      call save_grid()
+      !call save_grid(grid)
       call integrate_normals(sphere1)
 
 
       contains
-
-      ! general grid operations
-      subroutine initialize_grid()
-            integer :: i,j
-            do j = 1, n
-                  do i = 1, n
-                        grid(i,j) = 0.0_K
-                  end do
-            end do
-      end
-
-      subroutine save_grid()
-            integer :: i,j
-            real(K) :: axis_value(2)
-            open(unit=file_number, file='grid.dat', status='replace')
-            do j = 1, n
-                  do i = 1, n
-                        write(file_number, '(f10.3)',advance='no') grid(i,j)
-                        write(file_number, '(a)', advance='no') ' '
-                  end do
-                  write(file_number,*)
-            end do
-            close(file_number)
-
-            open(unit=file_number,file='x_axis.dat', status='replace')
-            do i = 1, n
-                  axis_value = x((/i,1/))
-                  write(file_number, *) axis_value(1)
-            end do
-            close(file_number)
-
-            open(unit=file_number,file='y_axis.dat', status='replace')
-            do j = 1, n
-                  axis_value = x((/1,j/))
-                  write(file_number, *) axis_value(2)
-            end do
-            close(file_number)
-
-      end
-
-      function x(integer_position)
-            integer, dimension(2) :: integer_position
-            real(K), dimension(2) :: x
-            
-            x(1) = -xL/2.0_K + xL*(integer_position(1)-1)/(n-1)
-            x(2) = -yL/2.0_K + yL*(integer_position(2)-1)/(n-1)
-      end
-
-      function xint(x)
-            real(K), dimension(2) :: x
-            integer, dimension(2) :: xint
-
-            xint(1) = (x(1)/xL + 0.5_K)*(n-1)+1
-            xint(2) = (x(1)/xL + 0.5_K)*(n-1)+1
-      end 
       
       ! sphere grid operations
       subroutine make_dip(obj)
@@ -117,17 +66,16 @@ program cheerios
             jL = obj%intcoord(2)-radius_int
             jH = obj%intcoord(2)+radius_int
             
-            open(unit=file_number,file='sphere_dip.dat',status='replace')
             do j = jL, jH
                   do i = iL, iH
-                        pos = x((/i,j/))
+                        pos = xpos((/i,j/),grid)
                         h = obj%radius**2 - (pos(1)-obj%coordinates(1))**2 &
                                           - (pos(2)-obj%coordinates(2))**2
                         h = min(0.0_K,obj%height - sqrt(h))
                         grid(i,j) = h
                   end do
             end do
-            close(file_number)
+            print *, 'dip made'
       end
       
       real(K) function capillary_dip(obj,pos)
@@ -164,7 +112,7 @@ program cheerios
             open(unit=file_number,file='normal.dat',status='replace')
             do j = jL, jH
                   do i = iL, iH
-                        pos = x((/i,j/))
+                        pos = xpos((/i,j/),grid)
                         h = obj%radius**2 - (pos(1)-obj%coordinates(1))**2 &
                                           - (pos(2)-obj%coordinates(2))**2
                         h = obj%height - sqrt(h)
